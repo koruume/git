2fff5244fac3229cf6f496e46e120cdd44ecd7c9
Jake Tangco <koruume@gmail.com>
2017-01-21 00:45:02 +0800

    new updates

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>haha</title>
-</head>
-<body>
-	<ul>
-		<li>piso</li>
-		<li>lima</li>
-		<li>pulo</li>
-	</ul>
-</body>
-</html>
+change 1
+change 2
+change 3
